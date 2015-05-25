d26e0aee77aa3d2ef797019185fe79af10ee211c
Giorgi Kikolashvili <gkiko10@freeuni.edu.ge>
2015-05-26 01:21:05 +0400

    trying to solve #2

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 var express = require('express');
 var app = express();
 
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 3000;
+
 app.set('view engine', 'jade');
 app.use(express.static('public'));
 
-var server = app.listen(3000, '0.0.0.0',function () {
+
+app.get('/', function(req, res){
+    res.render('home', {header1: 'Free Uni sdp 2015 final project'} );
+});
+
+var server = app.listen(port, '0.0.0.0',function () {
 
   var host = server.address().address;
   var port = server.address().port;
 
   console.log('xo-webui listening at http://%s:%s', host, port);
 
-});
-
-app.get('/', function(req, res){
-    res.render('home', {header1: 'Free Uni sdp 2015 final project'} );
 });
