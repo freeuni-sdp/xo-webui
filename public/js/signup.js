ef97f1f37e6a3207bc370053cc57e5678417382d
gkiko <gkiko10@freeuni.edu.ge>
2015-06-08 12:24:43 +0400

    resolves #10

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,17 +1,19 @@
 
 $(function(){
+  
+  $( "form" ).validate({
+    rules: {
+      password2: {
+          equalTo: "#p1"
+      }
+    }
+  });
+
   $( "form" ).submit(function(e) {
 
     $.ajax({
       type: "POST",
       url: "xo-signin.herokuapp.com/webui/sign",
-      beforeSend: function( xhr ) {
-        var pass1 = $( "input[name='password1']" ).val();
-        var pass2 = $( "input[name='password2']" ).val();
-        if (pass1 !== pass2) {
-          xhr.abort();
-        };
-      },
       dataType: 'jsonp',
       data: JSON.stringify($('form').serializeObject()),
       success: function(data) {
