c616b4df979d3812d57fea22f79e20fd7bf1ddd6
Giorgi Kikolashvili <gkiko10@freeuni.edu.ge>
2015-06-13 19:05:29 +0400

    compatible with api version freeuni-sdp/xo-rooms#9

--- public/js/rooms.js
+++ public/js/rooms.js
@@ -24,7 +24,7 @@ function drawGrid(data) {
     $('.row').last().append(
       $('<div/>', {'class':'col'})
       .append( $('<h3/>').append('#'+obj.room) )
-      .append( $('<h5/>') ).append('active users: [' + obj["x-user"]+', '+obj["o-user"] + ']')
+      .append( $('<h5/>') ).append('active users: [' + obj.x_user+', '+obj.o_user + ']')
       );
 
     console.log("#"+i+" "+obj.room);
