51dc683b937b117bfd70e9cb132188cc4faa427b
Giorgi Kikolashvili <gkiko10@freeuni.edu.ge>
2015-06-17 00:56:18 +0400

    closes #17. freeuni-sdp/xo-signin#9 still active

--- public/js/signup.js
+++ public/js/signup.js
@@ -6,24 +6,23 @@ $(function(){
       password2: {
           equalTo: "#p1"
       }
+    },
+    submitHandler: function(e) {
+
+      $.ajax({
+        type: "POST",
+        url: "http://private-6e6f0-xosignin.apiary-mock.com/webapi/signup",
+        dataType: 'json',
+        data: JSON.stringify($('form').serializeObject()),
+        success: function(data) {
+          alert(data); // show response
+        },
+        error: function(data) {
+          console.log("fail");
+        }
+      });
+      
     }
   });
 
-  $( "form" ).submit(function(e) {
-
-    $.ajax({
-      type: "POST",
-      url: "xo-signin.herokuapp.com/webui/sign",
-      dataType: 'jsonp',
-      data: JSON.stringify($('form').serializeObject()),
-      success: function(data) {
-        alert(data); // show response
-      },
-      error: function(data) {
-        console.log("fail");
-      }
-    });
-    
-    e.preventDefault();
-  });
 });
