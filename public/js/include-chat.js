dc4afc5487370ba0d35ef00c203b485ab9994c19
gkiko <gkiko10@freeuni.edu.ge>
2015-07-09 17:26:42 +0400

    disp;ay chat messages as list #22

--- public/js/include-chat.js
+++ public/js/include-chat.js
@@ -27,9 +27,43 @@ function displayChatWindow() {
 }
 
 function loadMessages() {
-    
+    $.ajax({
+        url: 'http://private-1a7f4-kukutodocore.apiary-mock.com/webapi/todos?token='+token,
+        type: 'GET',
+        success: function(data, status, xhttp) {
+            displayChatMessages($('#chat-list ul'), data);
+        },
+        error: function(data, status, xhttp) {
+            console.log("error "+status);
+        }
+    });
 }
 
 function removeChatWindow() {
     $('chatwindow').remove();
+}
+
+function displayChatMessages($parent, data) {
+    $.each(data, function( i, obj ) {
+        var rec = ChatRecord.toRecord(obj);
+        console.log(rec.html());
+        $parent.append(rec.html());
+
+    });
+}
+
+function ChatRecord(data) {
+    this.user = data.id;
+    this.message = data.text;
+
+    this.html = function() {
+        return $('<li/>', {'class':'msg-record'})
+            .append( $('<h5/>').append(this.user) )
+            .append( $('<h5/>').append(this.message) );
+    };
+
+}
+
+ChatRecord.toRecord = function(plain) {
+    return new ChatRecord(plain);
 }
